b8295372eefd0bdec8b5798307913594b8c89f9b
Huan Wang <wang.huan@northeastern.edu>
2021-07-08 11:21:43 -0400

    added scripts

---
+++ scripts/nerfv2_lego.sh
@@ -0,0 +1,18 @@
+# Train: nerf_v2
+CUDA_VISIBLE_DEVICES=1 python run_nerf_v2.py --config configs/lego.txt --n_sample_per_ray 4 --netwidth 1024 --netdepth 32 --skips 8,16,24 --directly_predict_rgb --teacher_ckpt Experiments/nerf__lego_SERVER-20210613-195444/blender_paper_lego/200000.tar --n_pose_kd 100 --kd_with_render_pose_mode all_render_pose --kd_poses_update 10000 --n_pose_video 100 --video_poses_perturb --N_iters 400000 --N_rand 16384 --precrop_iters -1 --pretrained_ckpt Experiments/nerfv2__lego__S4W1024D32Skip8,16,24_DPRGB_KDWRenderPose100All_BS16384_SERVER142-20210704-150540/weights/200000.tar --resume --project nerfv2__lego__S4W1024D32Skip8,16,24_DPRGB_KDWRenderPose100All_BS16384__Resume150540Iter400000
+
+CUDA_VISIBLE_DEVICES=1 nohup python run_nerf_v2.py --config configs/lego.txt --n_sample_per_ray 4 --netwidth 1024 --netdepth 32 --skips 8,16,24 --directly_predict_rgb --teacher_ckpt Experiments/nerf__lego_SERVER-20210613-195444/blender_paper_lego/200000.tar --n_pose_kd 100,25 --kd_with_render_pose_mode all_render_pose --n_pose_video 40,3 --video_poses_perturb --N_iters 600000 --N_rand 16384 --precrop_iters -1 --project nerfv2__lego__S4W1024D32Skip8,16,24_DPRGB_KDWRenderPose100,25All_BS16384 > /dev/null &
+
+CUDA_VISIBLE_DEVICES=2 nohup python run_nerf_v2.py --config configs/lego.txt --n_sample_per_ray 4 --netwidth 1024 --netdepth 32 --skips 8,16,24 --directly_predict_rgb --teacher_ckpt Experiments/nerf__lego_SERVER-20210613-195444/blender_paper_lego/200000.tar --n_pose_kd 20,5 --kd_with_render_pose_mode all_render_pose --n_pose_video 40,3 --video_poses_perturb --N_iters 600000 --N_rand 16384 --precrop_iters -1 --project nerfv2__lego__S4W1024D32Skip8,16,24_DPRGB_KDWRenderPose20,5All_BS16384 > /dev/null &
+
+
+
+
+# Test: nerf
+CUDA_VISIBLE_DEVICES=0 python run_nerf_v2.py --config configs/lego.txt --model_name nerf --pretrained_ckpt Experiments/nerf__lego_SERVER-20210613-195444/blender_paper_lego/200000.tar --render_only --n_pose_video 10 --debug
+
+
+# Test: nerf_v2
+CUDA_VISIBLE_DEVICES=1 python run_nerf_v2.py --config configs/lego.txt --render_only --n_sample_per_ray 4 --netwidth 1024 --netdepth 32 --skips 8,16,24 --directly_predict_rgb --n_pose_video 40 --video_poses_perturb --pretrained_ckpt Experiments/*-213124/weights/400000.tar --debug
+
+
