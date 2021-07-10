a9ca5f721f098cf07268257dbcc4d9e057307b20
Huan Wang <wang.huan@northeastern.edu>
2021-07-10 12:27:38 -0400

    update script

--- scripts/nerfv2_lego.sh
+++ scripts/nerfv2_lego.sh
@@ -6,13 +6,16 @@ CUDA_VISIBLE_DEVICES=1 nohup python run_nerf_v2.py --config configs/lego.txt --n
 CUDA_VISIBLE_DEVICES=2 nohup python run_nerf_v2.py --config configs/lego.txt --n_sample_per_ray 4 --netwidth 1024 --netdepth 32 --skips 8,16,24 --directly_predict_rgb --teacher_ckpt Experiments/nerf__lego_SERVER-20210613-195444/blender_paper_lego/200000.tar --n_pose_kd 20,5 --kd_with_render_pose_mode all_render_pose --n_pose_video 40,3 --video_poses_perturb --N_iters 600000 --N_rand 16384 --precrop_iters -1 --project nerfv2__lego__S4W1024D32Skip8,16,24_DPRGB_KDWRenderPose20,5All_BS16384 > /dev/null &
 
 
+# uniformly sample all angles, radiuses
+CUDA_VISIBLE_DEVICES=2 python run_nerf_v2.py --config configs/lego.txt --n_sample_per_ray 4 --netwidth 1024 --netdepth 32 --skips 8,16,24 --directly_predict_rgb --teacher_ckpt Experiments/nerf__lego_SERVER-20210613-195444/blender_paper_lego/200000.tar --kd_with_render_pose_mode all_render_pose --n_pose_kd 20,4,3 --kd_poses_update 10000 --n_pose_video 20,4,3 --N_iters 600000 --N_rand 16384 --pretrained_ckpt Exp*/*-014735/weights/600000.tar --precrop_iters -1 --project nerfv2__lego__S4W1024D32Skip8,16,24_DPRGB_KDAll_BS16384_NPose20,4,3Update10000 --test_pretrained --debug
+
 
 
 # Test: nerf
-CUDA_VISIBLE_DEVICES=0 python run_nerf_v2.py --config configs/lego.txt --model_name nerf --pretrained_ckpt Experiments/nerf__lego_SERVER-20210613-195444/blender_paper_lego/200000.tar --render_only --n_pose_video 10 --debug
+CUDA_VISIBLE_DEVICES=0 python run_nerf_v2.py --config configs/lego.txt --model_name nerf --pretrained_ckpt Experiments/nerf__lego_SERVER-20210613-195444/blender_paper_lego/200000.tar --render_only --n_pose_video 10,4,3 --debug
 
 
 # Test: nerf_v2
-CUDA_VISIBLE_DEVICES=1 python run_nerf_v2.py --config configs/lego.txt --render_only --n_sample_per_ray 4 --netwidth 1024 --netdepth 32 --skips 8,16,24 --directly_predict_rgb --n_pose_video 40 --video_poses_perturb --pretrained_ckpt Experiments/*-213124/weights/400000.tar --debug
+CUDA_VISIBLE_DEVICES=1 python run_nerf_v2.py --config configs/lego.txt --render_only --n_sample_per_ray 4 --netwidth 1024 --netdepth 32 --skips 8,16,24 --directly_predict_rgb --n_pose_video 20,4,3 --pretrained_ckpt Experiments/*-014735/weights/400000.tar --debug
 
 
