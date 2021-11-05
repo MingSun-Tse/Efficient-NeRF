452f7e7a72521b4eb91be2339bbd850fc0345459
Huan Wang <wang.huan@northeastern.edu>
2021-11-05 12:21:19 -0400

    added script: scp_teachers_blender.sh

---
+++ scripts/scp_teachers_blender.sh
@@ -0,0 +1,8 @@
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/nerf__chair_S* Experiments
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/nerf__drums_S* Experiments
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/nerf__ficus_S* Experiments
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/nerf__hotdog_S* Experiments
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/nerf__lego_S* Experiments
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/nerf__materials_S* Experiments
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/nerf__mic_S* Experiments
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/nerf__ship_S* Experiments
