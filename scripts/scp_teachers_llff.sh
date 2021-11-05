f55f0c7e21cc5c5ff034de574b436dd936272c11
Huan Wang <huanw@zju.edu.cn>
2021-11-04 21:24:06 -0400

    add scp_teachers_llff.sh and mv prepare_data.sh to scripts

---
+++ scripts/scp_teachers_llff.sh
@@ -0,0 +1,8 @@
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/*-121931 Experiments
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/*-121854 Experiments
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/*-125237 Experiments
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/*-125239 Experiments
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/*-122721 Experiments
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/*-202337 Experiments
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/*-202357 Experiments
+sshpass -p "tmp" scp -r wanghuan@155.33.199.5:/home/wanghuan/Projects/Efficient-NeRF/Experiments/*-214300 Experiments
