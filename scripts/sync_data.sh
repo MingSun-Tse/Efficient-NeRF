8efbae913ba2d2ec07504bc231e888120fbe9f47
Huan Wang <wang.huan@northeastern.edu>
2021-11-13 10:37:20 -0500

    added new script: sync_data.sh

--- scripts/sync_data.sh
+++ scripts/sync_data.sh
@@ -1,6 +1,6 @@
-rsync -au xuma@155.33.199.236:~/Projects/DONERF/src/data/classroom_v8_Rand_Origins_Dirs_4096RaysPerNpy_10kImages data/donerf_data/classroom_v8_Rand_Origins_Dirs_4096RaysPerNpy_10kImages 
+rsync -au xuma@155.33.197.144:~/Projects/DONERF/src/data/classroom_v8_Rand_Origins_Dirs_4096RaysPerNpy_10kImages data/donerf_data/classroom_v8_Rand_Origins_Dirs_4096RaysPerNpy_10kImages 
 
-rsync -au xuma@155.33.199.236:~/Projects/DONERF/src/data/sanmiguel_v8_Rand_Origins_Dirs_4096RaysPerNpy_10kImages data/donerf_data/sanmiguel_v8_Rand_Origins_Dirs_4096RaysPerNpy_10kImages
+rsync -au xuma@155.33.197.144:~/Projects/DONERF/src/data/sanmiguel_v8_Rand_Origins_Dirs_4096RaysPerNpy_10kImages data/donerf_data/sanmiguel_v8_Rand_Origins_Dirs_4096RaysPerNpy_10kImages
 
 rsync -au wanghuan@155.33.198.138:~/Projects/DONERF/src/data/pavillon_v8_Rand_Origins_Dirs_4096RaysPerNpy_10kImages data/donerf_data/pavillon_v8_Rand_Origins_Dirs_4096RaysPerNpy_10kImages
 
