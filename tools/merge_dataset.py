b0cf84c7c07b5489717f4aebfc47393f4c83ebe4
Huan Wang <huanw@zju.edu.cn>
2022-03-03 00:09:25 -0500

    Add tool: merge_dataset.py

--- tools/merge_dataset.py
+++ tools/merge_dataset.py
@@ -1,18 +1,21 @@
 import os, sys
+from multiprocessing import Pool
 inDir1 = os.path.abspath(sys.argv[1])
 inDir2 = os.path.abspath(sys.argv[2])
 outDir = os.path.abspath(sys.argv[3])
+cnt = [0]
+
+def fn(x):
+    os.symlink(x, os.path.split(x)[-1])
+    cnt[0] += 1
+    print(f'{cnt[0]}: {x}')
 
 def create_softlink(inDir):
-    for x in os.listdir(inDir):
-        x = os.path.join(inDir, x)
-        if os.path.isfile(x):
-            script = f'ln -s {x} .'
-            os.system(script)
-            print(script)
+    files = [os.path.join(inDir, x) for x in os.listdir(inDir)]
+    pool = Pool()
+    pool.map(fn, files)
 
 os.makedirs(outDir)
 os.chdir(outDir)
-create_softlink(inDir1)
 create_softlink(inDir2)
-
+create_softlink(inDir1)
