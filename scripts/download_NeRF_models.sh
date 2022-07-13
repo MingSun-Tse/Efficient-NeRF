71e097f6c95df743ca6b45ef3d34924380308ca1
Huan Wang <huanw@zju.edu.cn>
2022-07-13 14:06:59 -0400

    Update README

--- scripts/download_NeRF_models.sh
+++ scripts/download_NeRF_models.sh
@@ -1,2 +1,2 @@
-wget https://github.com/MingSun-Tse/Efficient-NeRF/releases/download/v0.1/NeRF_Blender_Models.zip
+wget https://github.com/MingSun-Tse/Efficient-NeRF/releases/download/v0.2/NeRF_Blender_Models.zip
 unzip NeRF_Blender_Models.zip
--- scripts/download_R2L_models.sh
+++ scripts/download_R2L_models.sh
@@ -1,2 +1,2 @@
-wget https://github.com/MingSun-Tse/Efficient-NeRF/releases/download/v0.1/R2L_Blender_Models.zip
+wget https://github.com/MingSun-Tse/Efficient-NeRF/releases/download/v0.2/R2L_Blender_Models.zip
 unzip R2L_Blender_Models.zip
