82cc8fae0ffced50887d75cd8f886de6da852375
Huan Wang <huanw@zju.edu.cn>
2023-07-29 15:54:38 -0400

    Update smilelogging; use softlink for scripts

--- scripts/download_NeRF_models.sh
+++
@@ -1,2 +0,0 @@
-wget https://github.com/MingSun-Tse/Efficient-NeRF/releases/download/v0.2/NeRF_Blender_Models.zip
-unzip NeRF_Blender_Models.zip
--- scripts/download_example_data.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p data
-cd data
-wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/nerf_example_data.zip
-unzip nerf_example_data.zip
-cd ..
